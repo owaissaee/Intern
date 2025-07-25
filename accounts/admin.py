064e85f2f7b8d489608d28966b7e466b2d87c23d
owaissaee <owaissaeed051@gmail.com>
2025-07-25 16:16:56 +0500

    ..

---
+++ accounts/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ['email', 'username', 'first_name', 'last_name', 'role', 'is_staff', 'is_active']
+    list_filter = ['role', 'is_staff', 'is_active', 'date_joined']
+    search_fields = ['email', 'username', 'first_name', 'last_name']
+    ordering = ['email']
+    
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('username', 'first_name', 'last_name', 'phone')}),
+        ('Permissions', {'fields': ('role', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'first_name', 'last_name', 'password1', 'password2', 'role'),
+        }),
+    )
